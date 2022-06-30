23ff009418313572176255993ef18c09983598bd
barbaros <hbarbaros06@gmail.com>
2022-06-30 23:48:19 +0200

    Base Page

--- src/test/java/com/loginFunction/pages/BasePage.java
+++ src/test/java/com/loginFunction/pages/BasePage.java
@@ -1,4 +1,18 @@
 package com.loginFunction.pages;
 
+import com.loginFunction.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /*public void getLink(String link){
+        Driver.getDriver().findElement(By.partialLinkText(link)).click();
+    }
+     */
+
 }
